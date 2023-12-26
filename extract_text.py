bb04128e55f6ff9f82811e89b25ecdbb3ca5bbb2
Ben Golder <bgolder2@codeforamerica.org>
2023-12-26 11:18:49 -0800

    Identify column and font

--- extract_text.py
+++ extract_text.py
@@ -1,27 +1,29 @@
+import re
+
 from pypdf import PdfReader
 from collections import namedtuple
 
 ClassSchedule = namedtuple("ClassSchedule", "campus path start_page end_page")
 
 schedules = [
-    ClassSchedule(
-        "Berkeley City College",
-        "pdfs/BCC Spring 24 Schedule V2.pdf",
-        49,
-        88,
-    ),
-    ClassSchedule(
-        "Laney College",
-        "pdfs/Laney Spring 24 Schedule V2.pdf",
-        52,
-        133,
-    ),
-    ClassSchedule(
-        "College of Alameda",
-        "pdfs/COA Spring 24 Schedule V2.pdf",
-        45,
-        81,
-    ),
+    # ClassSchedule(
+    #     "Berkeley City College",
+    #     "pdfs/BCC Spring 24 Schedule V2.pdf",
+    #     49,
+    #     88,
+    # ),
+    # ClassSchedule(
+    #     "Laney College",
+    #     "pdfs/Laney Spring 24 Schedule V2.pdf",
+    #     52,
+    #     133,
+    # ),
+    # ClassSchedule(
+    #     "College of Alameda",
+    #     "pdfs/COA Spring 24 Schedule V2.pdf",
+    #     45,
+    #     81,
+    # ),
     ClassSchedule(
         "Merritt College",
         "pdfs/Merritt Spring 24 Schedule V2.pdf",
@@ -35,38 +37,46 @@ def nice_int(n, width=3):
     return str(int(n)).rjust(width, " ")
 
 
-last_x = 0
-last_y = 0
+last_page = 0
+last_column = 0
+COL_X_THRESHOLD = 300
+CONTENT_Y_TOP = 743
+CONTENT_Y_BOTTOM = 24
 
 
-def visitor(text, um, tm, font_dict, font_size):
-    global last_x
-    global last_y
-    if text.strip():
+def is_primary_content(y):
+    return CONTENT_Y_BOTTOM < y < CONTENT_Y_TOP
+
+
+def visitor_for_page(page):
+    def visitor(text, um, tm, font_dict, font_size):
+        global last_page
+        global last_column
         x = tm[-2]
         y = tm[-1]
-        dx = x - last_x
-        dy = y - last_y
-        last_x = x
-        last_y = y
-        d = abs(dx * dy)
-        if d > 100000:  # and 10 < x < 100:
+        if text.strip() and is_primary_content(y):
+            font = font_dict["/BaseFont"][8:]
+            column = 1 if x < COL_X_THRESHOLD else 2
+            if column != last_column:
+                print(f"Switching to column {column}")
+                last_column = column
             print(
+                f"P: {page + 1}",
                 nice_int(x),
                 nice_int(y),
-                nice_int(d, 4),
-                nice_int(dx, 4),
-                nice_int(dy, 4),
                 [text],
+                font,
             )
-        return text
+            return text
+
+    return visitor
 
 
 for schedule in schedules:
     slug = "-".join([schedule.campus, "2024", "Spring"]).lower().replace(" ", "_")
     reader = PdfReader(schedule.path)
     with open(f"txts/{slug}.txt", "w", encoding="utf-8") as f:
-        # for i in range(schedule.start_page, schedule.start_page + 5):
-        for i in range(schedule.start_page, schedule.end_page + 1):
+        # for i in range(schedule.start_page - 1, schedule.end_page):
+        for i in range(schedule.start_page - 1, schedule.start_page + 2):
             f.write(f"\n\n$ PAGE {i + 1}\n\n")
-            f.write(reader.pages[i].extract_text(visitor_text=visitor))
+            f.write(reader.pages[i].extract_text(visitor_text=visitor_for_page(i)))
